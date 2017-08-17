10c6eed5b8a01078b379122e1d70ab16e81d0c76
Andrej Aderhold <andrej.aderhold@protonmail.com>
2017-08-17 17:53:47 +0200

    Added new data files to directly load from app.

---
+++ R/uiSession.R
@@ -0,0 +1,23 @@
+
+
+uiSession <- function() {
+  
+  fluidPage(
+    
+    box(width = 5, title = "Save Session", solidHeader = TRUE, status = "primary",
+        
+      "Save and download the current Analysis session to a .Rdata file.",
+      
+      div(style = "margin-top: 10px",
+            textInput("session_filename", "Filename", value = paste("GWSDAT_", gsub(":", "_", gsub(" ", "_", Sys.time())), ".Rdata", sep = ""))
+      ),
+      
+      div(style = "float: right",
+          downloadButton("save_session_btn", label = "Download", icon = icon("save"))
+      )
+                
+    )
+    
+  )
+  
+}
