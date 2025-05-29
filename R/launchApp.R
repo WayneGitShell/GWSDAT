d803c17524dad889a40b0de38c56fabc8dc8fce6
WayneGitShell <wayne.w.jones@shell.com>
2025-05-29 16:31:34 +0100

    Only change Shiny user agg to false if not specified.

--- R/launchApp.R
+++ R/launchApp.R
@@ -41,6 +41,7 @@ launchApp <- function(GWSDAT_Options, session_file) {
   if (missing(GWSDAT_Options) && missing(session_file)) {
     
     .GlobalEnv$APP_RUN_MODE <- "MultiData"
+    options(shiny.useragg = FALSE) #to avoid artefacts in spatial plots
     
     shinyApp(ui = uiFull(), server = server)
     
@@ -65,6 +66,7 @@ launchApp <- function(GWSDAT_Options, session_file) {
     }
     
     options(shiny.launch.browser = TRUE)
+    options(shiny.useragg = FALSE) #to avoid artefacts in spatial plots
     
     shinyApp(ui = uiSimple(), server = server)
   }
